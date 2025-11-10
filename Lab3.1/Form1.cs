5799f427c4b958999982eee21cb3bdc83f1b01c6
Klein David <146858982+KDT19@users.noreply.github.com>
2025-11-10 17:52:48 +0200

    Add files via upload

---
+++ Lab3.1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3._1
+{
+    public partial class Form1 : Form
+    {
+        Point[] triunghiOriginal;
+        Point[] triunghiTranslatat;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Translație Triunghi";
+            this.Size = new Size(400, 400);
+            this.Paint += Form_Paint;
+            triunghiOriginal = new Point[]
+            {
+                new Point(50, 50),
+                new Point(150, 50),
+                new Point(100, 150)
+            };
+            int dx = 2;
+            int dy = 3;
+            triunghiTranslatat = new Point[triunghiOriginal.Length];
+            for (int i = 0; i < triunghiOriginal.Length; i++)
+            {
+                triunghiTranslatat[i] = new Point(triunghiOriginal[i].X + dx, triunghiOriginal[i].Y + dy);
+            }
+        }
+        private void Form_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.FillPolygon(Brushes.Red, triunghiOriginal);
+            g.DrawPolygon(Pens.Black, triunghiOriginal);
+            g.FillPolygon(Brushes.Blue, triunghiTranslatat);
+            g.DrawPolygon(Pens.Black, triunghiTranslatat);
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new Form());
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
