dbbe71154b548cc4a038ae6c2bc45e9183239626
Klein David <146858982+KDT19@users.noreply.github.com>
2025-11-10 17:56:44 +0200

    Add files via upload

---
+++ lab4.1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4._1
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> points = new List<Point>();
+        private bool polygonCompleted = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Scanline Poligon Interactiv";
+            this.Size = new Size(600, 600);
+            this.BackColor = Color.White;
+            this.MouseClick += Form1_MouseClick;
+            this.KeyDown += Form1_KeyDown;
+            this.DoubleBuffered = true;
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (polygonCompleted)
+                return;
+            if (e.Button == MouseButtons.Left)
+            {
+                points.Add(e.Location);
+                this.Invalidate();
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && points.Count >= 3 && !polygonCompleted)
+            {
+                polygonCompleted = true;
+                this.Invalidate();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            foreach (var p in points)
+            {
+                g.FillEllipse(Brushes.Red, p.X - 4, p.Y - 4, 8, 8);
+            }
+            if (points.Count > 1)
+            {
+                Pen pen = new Pen(Color.Black, 2);
+                for (int i = 0; i < points.Count - 1; i++)
+                {
+                    g.DrawLine(pen, points[i], points[i + 1]);
+                }
+
+                if (polygonCompleted)
+                {
+                    g.DrawLine(pen, points[points.Count - 1], points[0]);
+                    Brush brush = new SolidBrush(Color.LightBlue);
+                    g.FillPolygon(brush, points.ToArray());
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ lab5.1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5._1
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalImage;
+        private Bitmap clippedImage;
+        private Rectangle clipRect;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 800;
+            this.Height = 600;
+            this.Text = "Clipping imagine - exemplu C#";
+            LoadImageWithDialog();
+            clipRect = new Rectangle(100, 100, 200, 150);
+            if (originalImage != null)
+                clippedImage = ClipImage(originalImage, clipRect);
+            this.Paint += Form1_Paint;
+        }
+        private void LoadImageWithDialog()
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Title = "Alege o imagine";
+                ofd.Filter = "Fișiere imagine|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    originalImage = new Bitmap(ofd.FileName);
+                }
+                else
+                {
+                    MessageBox.Show("Nu ai ales nicio imagine. Se va închide aplicația.");
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la încărcarea imaginii: " + ex.Message);
+                this.Close();
+            }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (originalImage == null)
+                return;
+            g.DrawImage(originalImage, 10, 10, 300, 200);
+            g.DrawRectangle(Pens.Red, 10 + clipRect.X, 10 + clipRect.Y, clipRect.Width, clipRect.Height);
+            if (clippedImage != null)
+                g.DrawImage(clippedImage, 350, 50);
+        }
+        private Bitmap ClipImage(Bitmap src, Rectangle area)
+        {
+            Bitmap bmp = new Bitmap(area.Width, area.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.DrawImage(src, new Rectangle(0, 0, area.Width, area.Height),
+                    area,
+                    GraphicsUnit.Pixel);
+            }
+            return bmp;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
